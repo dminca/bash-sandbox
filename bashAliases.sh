97ef3bce0da27e5e785f69ef62bc1aaf79c4af4a
Minca Daniel Andrei <dminca@marketstheworld.com>
2015-12-11 14:31:46 +0200

    Successfully load & run bash aliases if they exist

--- bashAliases.sh
+++ bashAliases.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 shopt -s expand_aliases
 
-if [ -z "$HOME/.bash_aliases" ];
+if [ -f "$HOME/.bash_aliases" ];
 then
   source ~/.bash_aliases
+else
+  echo -e "\e[91m[ERROR]\e[0m Bash aliases config not found."
 fi
 
 we stop
